99cd30aee5b483b49c1ccc525135d366ae86339a
lcalero42 <luis.calero1777@gmail.com>
2026-04-23 08:58:46 +0200

    Task(server): server class skeleton (#26)
    * task(Server): added Server class with constructor and destructor and logger macro function
    
    * fix(server): fixed debug mode not working with make debug
    
    * style(server): normed the Server class to match with clang-format
    
    * refac(server): changed constructor parameter from const std::string to const reference string for opti
    
    * style(server): --format on main.cpp and Server.hpp
    
    ---------
    
    Co-authored-by: ekeisler <enzokeisler89@gmail.com>

---
+++ include/Server.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 16:27:59 by lcalero           #+#    #+#             */
+/*   Updated: 2026/04/22 21:00:13 by lcalero          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <vector>
+
+#define MAX_EVENTS 50
+
+#ifndef DEBUG
+#	define DEBUG 0
+#endif
+
+#if DEBUG
+#	define LOG_INFO(msg) std::cout << "[DEBUG] " << msg << std::endl
+#else
+#	define LOG_INFO(msg)                                                      \
+		do                                                                     \
+		{                                                                      \
+		} while (0)
+#endif
+
+class Server
+{
+	private:
+		int				  _epoll_fd;
+		int				  _listen_sock;
+		int				  _port;
+		const std::string _password;
+
+	public:
+		Server(int port, const std::string& password);
+		~Server();
+
+		// returns client's fd
+		int listen_sockets();
+};
+
+#endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,32 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/14 17:44:20 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/22 17:45:06 by lcalero          ###   ########.fr       */
+/*   Created: 2026/04/22 16:52:35 by lcalero           #+#    #+#             */
+/*   Updated: 2026/04/22 21:00:24 by lcalero          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
-#include <iostream>
-#include <sstream>
 
-int
-main(int argc, char** argv)
+Server::Server(int port, const std::string& password) :
+	_epoll_fd(-1),
+	_listen_sock(-1),
+	_port(port),
+	_password(password)
 {
-	if (argc != 3)
-	{
-		std::cerr << "Error: invalid number of arguments\n"
-				  << "Usage: " << argv[0] << " <port> <password>\n";
-		return (1);
-	}
-	std::stringstream ss;
-	int				  n;
-	ss << argv[1];
-	ss >> n;
-	Server serv(n, argv[2]);
-	return (0);
+	LOG_INFO(this->_port);
+	LOG_INFO(this->_password);
+	LOG_INFO(this->_epoll_fd);
+	LOG_INFO(this->_listen_sock);
+}
+
+Server::~Server()
+{
 }
