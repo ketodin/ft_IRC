9d9514241dc897212de8a7683865af8fafbc3e88
ketodin <69580109+ketodin@users.noreply.github.com>
2026-04-24 20:42:27 +0200

    Task/56 Finalise class interfaces for PassCommand, NickCommand, UserCommand (#86)
    * task(parsing): add empty header stubs for core command classes
    add empty header stubs for auth command classes
    add empty header stubs for channel command classes
    
    * add empty source stubs for core command classes
     add empty source stubs for auth command classes
     add empty source stubs for channel command classes
    add empty source stubs for operator command classes
    add utils.hpp and utils.cpp empty stubs
    
    * style(parsing): fixed stubs causing clang-format errors
    
    * chore(makefile): add auth.mk, channel.mk, operator.mk commands.mk
    added new include directories insude includes.mk
    edited srcs.mk to load commands/commands.mk
    
    * task(parser): finalise CommandParser class interface in header
    finalise CommandDispatcher class interface and HandlerMap typedef
    finalise ACommand base class interface with validation helpers
    
    * task(parsing/auth): finalise Pass, Nick, User Command class interface in headers

--- include/commands/auth/PassCommand.hpp
+++ include/commands/auth/PassCommand.hpp
@@ -6,11 +6,29 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:25:46 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:34:01 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/23 17:36:45 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PASS_COMMAND_HPP
 #define PASS_COMMAND_HPP
+
+#include "ACommand.hpp"
+#include <string>
+#include <vector>
+
+class PassCommand : public ACommand
+{
+	private:
+		PassCommand(void);
+		PassCommand(const PassCommand& other);
+		PassCommand& operator=(const PassCommand& other);
+
+	public:
+		static const std::string NAME; // = "PASS"
+
+		// PASS <password>
+		static void execute(const std::vector<std::string>& args);
+};
 
 #endif // PASS_COMMAND_HPP
