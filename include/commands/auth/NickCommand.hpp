b087aa306867ee3db6d95a62fe529e868555897a
jaubry-- <jaubry--@student.42lyon.fr>
2026-04-24 20:26:11 +0200

    task(parsing/auth): finalise Pass, Nick, User Command class interface in headers

--- include/commands/auth/NickCommand.hpp
+++ include/commands/auth/NickCommand.hpp
@@ -6,11 +6,29 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:25:49 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:33:56 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/23 21:36:43 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NICK_COMMAND_HPP
 #define NICK_COMMAND_HPP
+
+#include "ACommand.hpp"
+#include <string>
+#include <vector>
+
+class NickCommand : public ACommand
+{
+	private:
+		NickCommand(void);
+		NickCommand(const NickCommand& other);
+		NickCommand& operator=(const NickCommand& other);
+
+	public:
+		static const std::string NAME; // = "NICK"
+
+		// NICK <nickname>
+		static void execute(const std::vector<std::string>& args);
+};
 
 #endif // NICK_COMMAND_HPP
