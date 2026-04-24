1bdd088f997a5058adb10997dec50b4b8fe6c16a
ketodin <69580109+ketodin@users.noreply.github.com>
2026-04-24 20:47:25 +0200

    Task/57 Finalise class interfaces for JoinCommand and PrivmsgCommand (#88)
    * task(parsing): add empty header stubs for core command classes
    add empty header stubs for auth command classes
    add empty header stubs for channel command classes
    
    * add empty source stubs for core command classes
     add empty source stubs for auth command classes
     add empty source stubs for channel command classes
    add empty source stubs for operator command classes
    add utils.hpp and utils.cpp empty stubs
    
    * style(parsing): fixed stubs causing clang-format errors
    
    * chore(makefile): add auth.mk, channel.mk, operator.mk commands.mk
    added new include directories insude includes.mk
    edited srcs.mk to load commands/commands.mk
    
    * task(parser): finalise CommandParser class interface in header
    finalise CommandDispatcher class interface and HandlerMap typedef
    finalise ACommand base class interface with validation helpers
    
    * task(parsing/auth): finalise Pass, Nick, User Command class interface in headers
    
    * task(parsing/channel): finalise Join, Privmsg command class interface in headers

--- include/commands/channel/JoinCommand.hpp
+++ include/commands/channel/JoinCommand.hpp
@@ -6,11 +6,29 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:25:40 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:33:43 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/23 22:25:01 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef JOIN_COMMAND_HPP
 #define JOIN_COMMAND_HPP
+
+#include "ACommand.hpp"
+#include <string>
+#include <vector>
+
+class JoinCommand : public ACommand
+{
+	private:
+		JoinCommand(void);
+		JoinCommand(const JoinCommand& other);
+		JoinCommand& operator=(const JoinCommand& other);
+
+	public:
+		static const std::string NAME; // = "JOIN"
+
+		// JOIN <username>
+		static void execute(const std::vector<std::string>& args);
+};
 
 #endif // JOIN_COMMAND_HPP
--- include/commands/channel/PrivmsgCommand.hpp
+++ include/commands/channel/PrivmsgCommand.hpp
@@ -6,11 +6,29 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:25:21 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:33:48 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/23 22:25:32 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRIVMSG_COMMAND_HPP
 #define PRIVMSG_COMMAND_HPP
+
+#include "ACommand.hpp"
+#include <string>
+#include <vector>
+
+class PrivmsgCommand : public ACommand
+{
+	private:
+		PrivmsgCommand(void);
+		PrivmsgCommand(const PrivmsgCommand& other);
+		PrivmsgCommand& operator=(const PrivmsgCommand& other);
+
+	public:
+		static const std::string NAME; // = "PRIVMSG"
+
+		// PRIVMSG <username>
+		static void execute(const std::vector<std::string>& args);
+};
 
 #endif // PRIVMSG_COMMAND_HPP
