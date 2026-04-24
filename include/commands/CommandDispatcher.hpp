be298fab6181e742cb3b622c514579a0478dd6f9
jaubry-- <jaubry--@student.42lyon.fr>
2026-04-24 19:35:14 +0200

    task(parsing): add empty header stubs for core command classes add empty header stubs for auth command classes add empty header stubs for channel command classes

---
+++ include/commands/CommandDispatcher.hpp
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CommandDispatcher.hpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 16:24:50 by jaubry--          #+#    #+#             */
+/*   Updated: 2026/04/24 19:33:02 by jaubry--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMAND_DISPATCHER_HPP
+#define COMMAND_DISPATCHER_HPP
+
+#endif // COMMAND_DISPATCHER_HPP
