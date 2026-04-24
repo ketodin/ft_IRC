924c3d03782d0ca61a15e70a3971a406023364ae
jaubry-- <jaubry--@student.42lyon.fr>
2026-04-24 20:35:02 +0200

    task(parsing/operator): finalise Invite, Kick, Mode, Topic command class interface in headers

--- include/commands/operator/InviteCommand.hpp
+++ include/commands/operator/InviteCommand.hpp
@@ -6,11 +6,29 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:25:53 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:33:15 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/23 22:26:00 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INVITE_COMMAND_HPP
 #define INVITE_COMMAND_HPP
+
+#include "ACommand.hpp"
+#include <string>
+#include <vector>
+
+class InviteCommand : public ACommand
+{
+	private:
+		InviteCommand(void);
+		InviteCommand(const InviteCommand& other);
+		InviteCommand& operator=(const InviteCommand& other);
+
+	public:
+		static const std::string NAME; // = "INVITE"
+
+		// INVITE <username>
+		static void execute(const std::vector<std::string>& args);
+};
 
 #endif // INVITE_COMMAND_HPP
--- include/commands/operator/KickCommand.hpp
+++ include/commands/operator/KickCommand.hpp
@@ -6,11 +6,29 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:25:55 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:33:23 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/23 22:26:19 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KICK_COMMAND_HPP
 #define KICK_COMMAND_HPP
+
+#include "ACommand.hpp"
+#include <string>
+#include <vector>
+
+class KickCommand : public ACommand
+{
+	private:
+		KickCommand(void);
+		KickCommand(const KickCommand& other);
+		KickCommand& operator=(const KickCommand& other);
+
+	public:
+		static const std::string NAME; // = "KICK"
+
+		// KICK <username>
+		static void execute(const std::vector<std::string>& args);
+};
 
 #endif // KICK_COMMAND_HPP
