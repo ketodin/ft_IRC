32677e1ebea887b5cc325ee281d245f26a30b315
ketodin <69580109+ketodin@users.noreply.github.com>
2026-04-24 21:48:52 +0200

    Task/59 Implement shared utils helpers (is_digits, ft_atou, escape_str (#90)
    * task(parsing): add empty header stubs for core command classes
    add empty header stubs for auth command classes
    add empty header stubs for channel command classes
    
    * add empty source stubs for core command classes
     add empty source stubs for auth command classes
     add empty source stubs for channel command classes
    add empty source stubs for operator command classes
    add utils.hpp and utils.cpp empty stubs
    
    * style(parsing): fixed stubs causing clang-format errors
    
    * chore(makefile): add auth.mk, channel.mk, operator.mk commands.mk
    added new include directories insude includes.mk
    edited srcs.mk to load commands/commands.mk
    
    * task(parser): finalise CommandParser class interface in header
    finalise CommandDispatcher class interface and HandlerMap typedef
    finalise ACommand base class interface with validation helpers
    
    * task(parsing/auth): finalise Pass, Nick, User Command class interface in headers
    
    * task(parsing/channel): finalise Join, Privmsg command class interface in headers
    
    * task(parsing/operator): finalise Invite, Kick, Mode, Topic command class interface in headers
    
    * task(utils): implement is_digits and ft_atou helpers in utils
    implement escape_char and escape_str debug helpers in utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,12 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/24 19:42:09 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/24 19:46:07 by jaubry--         ###   ########.fr       */
+/*   Created: 2026/04/23 19:25:25 by jaubry--          #+#    #+#             */
+/*   Updated: 2026/04/23 20:09:26 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
+
+bool is_digits(const std::string& str);
+
+void ft_atou(const std::string& str, int& num);
+
+std::string escape_str(const std::string& str);
 
 #endif // UTILS_HPP
