e1c0887919656d4a3a0cc10f6bd4429c0797057d
ekeisler <enzokeisler89@gmail.com>
2026-04-28 18:25:14 +0200

    task(command): CAP Command working properly (#98)

--- src/commands/auth/CapCommand.cpp
+++ src/commands/auth/CapCommand.cpp
@@ -6,7 +6,7 @@
 /*   By: ekeisler <ekeisler@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 17:36:26 by ekeisler          #+#    #+#             */
-/*   Updated: 2026/04/27 19:27:42 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/04/28 18:23:26 by ekeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,8 @@ CapCommand::execute(Client& client, // cppcheck-suppress constParameterReference
 	requireArgsNum(args, 2, "CAP LS 302");
 	requireWord(args, 0, "CAP");
 
-	std::cout << "[CAP] Answering the client about the features implemented in "
-				 "the server : "
-			  << ":ircserv CAP * LS :\r\n"
-			  << std::endl;
-
 	std::cout << "Client fd: " << client.getFd();
-	if (!client.getIsLogged())
+	if (!client.isRegistered())
 	{
 		std::string r = ":ircserv CAP * LS :\r\n";
 		send(client.getFd(), r.c_str(), r.size(), 0);
