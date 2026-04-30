fe19bf3ddeaa916584415b93258545e64941b4c3
Luis Calero <luis.calero1777@gmail.com>
2026-04-30 06:03:33 +0200

    task(operator): implemented KICK command with all parsing and broadcast logic

--- src/commands/CommandDispatcher.cpp
+++ src/commands/CommandDispatcher.cpp
@@ -6,7 +6,7 @@
 /*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 17:11:23 by jaubry--          #+#    #+#             */
-/*   Updated: 2026/04/30 03:41:13 by lcalero          ###   ########.fr       */
+/*   Updated: 2026/04/30 05:08:53 by lcalero          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ CommandDispatcher::CommandDispatcher(void)
 	registerCommand(JoinCommand::NAME, &JoinCommand::execute);
 	registerCommand(TopicCommand::NAME, &TopicCommand::execute);
 	registerCommand(PrivmsgCommand::NAME, &PrivmsgCommand::execute);
+	registerCommand(KickCommand::NAME, &KickCommand::execute);
 
 	/*
 	registerCommand(JoinCommand::NAME, &JoinCommand::execute);
 	registerCommand(InviteCommand::NAME, &InviteCommand::execute);
-	registerCommand(KickCommand::NAME, &KickCommand::execute);
 	registerCommand(ModeCommand::NAME, &ModeCommand::execute);
 	*/
 }
